bfa06c498ca4fc2369665c63945c035bfb45c5c2
unknown <eranner@gmail.com>
2024-03-27 15:00:43 -0400

    removed form placeholders

--- themes/longwings/main-form.php
+++ themes/longwings/main-form.php
@@ -8,33 +8,33 @@
     <form class="form-content-holder">
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="name" placeholder="John Smith">
+            <input type="text" class="form-control" id="name" placeholder="">
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" placeholder="email@example.com">
+            <input type="email" class="form-control" id="email" placeholder="">
         </div>
         <div class="mb-3">
             <label for="address" class="form-label">Address</label>
-            <input type="text" class="form-control" id="address" placeholder="John Smith">
+            <input type="text" class="form-control" id="address" placeholder="">
         </div>
         <div class="mb-3" style="display:flex; gap: 5px;">
             <div style="display: flex; flex-direction: column;">
                 <label for="city" class="form-label">City</label>
-                <input type="text" class="form-control" id="city" placeholder="John Smith">
+                <input type="text" class="form-control" id="city" placeholder="">
             </div>
             <div style="display: flex; flex-direction: column;">
                 <label for="state" class="form-label">State</label>
-                <input type="text" class="form-control" id="state" placeholder="John Smith">
+                <input type="text" class="form-control" id="state" placeholder="">
             </div>
             <div style="display: flex; flex-direction: column;">
                 <label for="zip" class="form-label">Zip</label>
-                <input type="text" class="form-control" id="zip" placeholder="John Smith">
+                <input type="text" class="form-control" id="zip" placeholder="">
             </div>
         </div>
         <div class="mb-3">
             <label for="phone" class="form-label">Phone Number</label>
-            <input type="text" class="form-control" id="phone" placeholder="John Smith">
+            <input type="text" class="form-control" id="phone" placeholder="">
         </div>
         <div style="display:flex; justify-content:center;">
             <input type="submit" class="btn frontpage-card-button">
