73d0eb81b5b9e89169f763e14f3901774d862e15
Eric Ranner <eranner@Erics-MacBook-Pro.local>
2024-03-27 23:34:17 -0400

    added faculty cards

--- themes/longwings/functions.php
+++ themes/longwings/functions.php
@@ -37,3 +37,48 @@ function show_main_cards() {
   }
   
   add_shortcode('main_cards', 'show_main_cards');
+
+  function show_teachers() {
+    $teacher_cards = new WP_Query([
+        'posts_per_page' => -1,
+        'post_type' => 'teacher_cards',
+        'order' => 'ASC'
+      ]);
+
+      $output = '<div class="container" id="all-teachers-holder">';
+  
+      while ($teacher_cards->have_posts()) {
+        $teacher_cards->the_post();
+        $name = get_the_title();
+        $bio = get_field('bio');
+        $fact1 = get_field('first_fact');
+        $fact2 = get_field('second_fact');
+        $fact3 = get_field('third_fact');
+        $picture = esc_url(get_field('teacher_photo'));
+        $output .= '
+<div class="faculty-card">
+    <div class="faculty-image-holder">
+        <img src="'.$picture.'" alt="" class="img-fluid faculty-image">
+    </div>
+    <div class="faculty-bio">
+        <h3 class="facultyname">'.$name.'</h3>
+        <p class="bio-snippet">'.$bio.'</p>
+        <h5>Fun Facts</h5>
+        <ul class="faculty-accomplishments">
+            ';
+            if($fact1){$output.= "<li>$fact1</li>";}
+            if($fact2){$output.= "<li>$fact2</li>";}
+            if($fact3){$output.= "<li>$fact3</li>";}
+
+           $output.= '
+        </ul>
+    </div>
+</div>
+       </div> ';
+      }
+    
+      wp_reset_postdata(); // Reset post data after custom query
+    
+      return $output;
+  }
+  add_shortcode('teacher_cards', 'show_teachers');
