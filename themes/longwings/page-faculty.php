bc7752e47a3dd5120745a88c1bdac9fb4e235e9d
unknown <eranner@gmail.com>
2024-03-29 14:01:55 -0400

    added padding to employees container

--- themes/longwings/page-faculty.php
+++ themes/longwings/page-faculty.php
@@ -4,7 +4,7 @@ require('navbar.php');
 require('small-hero.php');
 ?>
 <div class="cardholders-main" >
-        <div class="main-card-cover" style="display: flex; flex-direction: column; align-items:center; padding-top: 40px;">
+        <div class="main-card-cover" style="display: flex; flex-direction: column; align-items:center; padding-top: 40px;padding-bottom:40px;">
 <div class="container" id="gutenberg-delivery-container" style="overflow:hidden;">
 
    <?php
