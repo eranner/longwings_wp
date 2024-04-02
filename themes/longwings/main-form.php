e0fb8163d2b202aa4c00a913edb6dbf7a1d120c6
Eric Ranner <eranner@Erics-MacBook-Pro.local>
2024-04-01 22:43:51 -0400

    updated main-form

--- themes/longwings/main-form.php
+++ themes/longwings/main-form.php
@@ -13,36 +13,36 @@
     <form class="form-content-holder" id="main-page-form">
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="name" placeholder="" required>
+            <input type="text" name="name" class="form-control" id="name" placeholder="" required>
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" placeholder="" required>
+            <input type="email" name="email" class="form-control" id="email" placeholder="" required>
         </div>
         <div class="mb-3">
             <label for="address" class="form-label">Address</label>
-            <input type="text" class="form-control" id="address" placeholder="" required>
+            <input type="text" name="address" class="form-control" id="address" placeholder="" required>
         </div>
         <div class="mb-3" style="display:flex; gap: 5px;">
             <div style="display: flex; flex-direction: column;">
                 <label for="city" class="form-label">City</label>
-                <input type="text" class="form-control" id="city" placeholder="" required>
+                <input type="text" name="city" class="form-control" id="city" placeholder="" required>
             </div>
             <div style="display: flex; flex-direction: column;">
                 <label for="state" class="form-label">State</label>
-                <input type="text" class="form-control" id="state" placeholder="" required>
+                <input type="text" name="state" class="form-control" id="state" placeholder="" required>
             </div>
             <div style="display: flex; flex-direction: column;">
                 <label for="zip" class="form-label">Zip</label>
-                <input type="text" class="form-control" id="zip" placeholder=""required>
+                <input type="text" name="zip"class="form-control" id="zip" placeholder=""required>
             </div>
         </div>
         <div class="mb-3">
             <label for="phone" class="form-label">Phone Number</label>
-            <input type="text" class="form-control" id="phone" placeholder="">
+            <input type="text" name="phone" class="form-control" id="phone" placeholder="">
         </div>
         <div style="display:flex; justify-content:center;">
-            <input type="submit" class="btn frontpage-card-button">
+            <input type="submit" id="email_list_builder_button" class="btn frontpage-card-button">
         </div>
 </form>
 </div>
