cef8aba12e95ece5fe1b905451d37a3a27f14dbd
Eric Ranner <eranner@gmail.com>
2024-04-18 21:41:19 -0400

    troubleshooting js

--- themes/longwings/main.js
+++ themes/longwings/main.js
@@ -1,5 +1,10 @@
-let donorbox = document.querySelector('.dbox-reminder-widget')
 
-if(donorbox){
-    donorbox.style.display = 'none';
-}
+document.addEventListener('DOMContentLoaded', ()=> {
+   console.log('event')
+    let donorbox = document.querySelector('.dbox-reminder-widget')
+
+    if(donorbox){
+        donorbox.style.display = 'none';
+        console.log('detected')
+    }
+})
